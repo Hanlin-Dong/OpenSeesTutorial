f78f3e08656d6684174956ad72fa4b3a2fd213e8
Hanlin <hanlin.dong@gmail.com>
2016-10-19 21:51:53 +0800

    add sublime.rst and made clean

--- conf.py
+++ conf.py
@@ -230,7 +230,7 @@ html_static_path = ['_static']
 #   'da', 'de', 'en', 'es', 'fi', 'fr', 'hu', 'it', 'ja'
 #   'nl', 'no', 'pt', 'ro', 'ru', 'sv', 'tr', 'zh'
 #
-# html_search_language = 'en'
+html_search_language = 'zh'
 
 # A dictionary with options for the search language support, empty by default.
 # 'ja' uses this config value.
